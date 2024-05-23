75d2ae04e23cdcc0f458ee3781b6e99769f1e0d7
Deng Shuanglin <shuanglindev@gmail.com>
2024-05-23 15:17:04 +0800

    修改astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
-	output: 'server',
-	integrations: [tailwind(), sitemap()],
-	adapter: vercel(),
+	// site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
+	// output: 'server',
+	// integrations: [ tailwind(), sitemap()],
+	integrations: [tailwind()],
+	// adapter: vercel(),
 });
