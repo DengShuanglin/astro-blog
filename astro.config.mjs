21c5c1674a5197dc21eecca24c173a7437e8f822
Deng Shuanglin <shuanglindev@gmail.com>
2025-12-19 20:54:06 +0800

    fix: format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	// site: 'https://astro-simple-portfolio.vercel.app/', // Required for sitemap -> Replace with your site's URL
 	// output: 'server',
 	// integrations: [ tailwind(), sitemap()],
-	integrations: [tailwind()],
 	// adapter: vercel(),
+	integrations: [tailwind()],
+	outDir: '/root/web/homepage',
 });
